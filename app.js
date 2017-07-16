8b6927af5bd34b8968b90a8ef9a46625d865541d
kingshark <kingshark8848@live.com>
2017-07-17 01:40:45 +1000

    user login page

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ http.listen(port, function () {
 })
 
 SOCKET_LIST = {};
+ALL_USERS = [];
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-
   socket.id = shortid.generate();
+  console.log('user '+socket.id+' connected');
   socket.name = socket.id;
   socket.img_url = app_url + ":" + port + '/img/avatar/1.jpg';
   SOCKET_LIST[socket.id] = socket;
@@ -30,11 +30,34 @@ io.on('connection', function(socket){
   // send socket info to client
   socket.emit('init_user', get_user(socket));
 
+  socket.on('change_user', function(data){
+  	console.log('user '+data.id+' changed!');
+
+  	// change data
+  	socket.name = data.name;
+
+  	// notify client
+  	socket.emit('change_user_done', get_user(socket));
+
+  })
 
   socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+  	delete SOCKET_LIST[socket.id];
+    console.log('user '+socket.id+' disconnected');
+  })
+
 })
+
+setInterval(function(){
+
+	ALL_USERS = get_all_users();
+
+	for (var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('update_all_users', ALL_USERS);
+	}
+
+},1000)
 
 function get_user(socket){
 	return {
@@ -42,6 +65,14 @@ function get_user(socket){
 		name: socket.name,
 		img_url: socket.img_url
 	}
+}
+
+function get_all_users(){
+	var all_users = [];
+	for (var i in SOCKET_LIST){
+		all_users.push(get_user(SOCKET_LIST[i]));
+	}
+	return all_users;
 }
 
 
