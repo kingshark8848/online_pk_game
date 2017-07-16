a25a93f103475fd88853f345b821f56d56e4fa3a
kingshark <kingshark8848@live.com>
2017-07-16 20:17:16 +1000

    add socket.io server

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http, {});
 
 // serve staic file
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
+
+
