ad61a1f6cc7f2a26106c1f16840502fabee8716c
kingshark <kingshark8848@live.com>
2017-07-16 20:35:44 +1000

    +sever communication

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http, {});
+var io = require('socket.io')(http);
 
 // serve staic file
 app.use(express.static('public'));
@@ -10,8 +10,19 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 })
 
-app.listen(3000, function () {
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+})
+
 
 
